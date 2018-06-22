a3a09d087a988434d5866cc09c844aaae25ed71d
asshah4 <mrshahman@gmail.com>
2018-06-22 19:13:32 -0400

    fixed posts to post, plurarility issue for paginator

--- _site/make-bib.sh
+++ _site/make-bib.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-# Bring in bibliography file for ultrasound project
-cp ../bibliography/ultrasound.bib .
+# Clean house
+rm *.bib
+
+# Bring in bibliography files that are clinically appropriate
+cp ../bibliography/medicine.bib .
+
+# Trim headers off of all files
+sed -i '' 1,5d *.bib
+
+# Move files into a tmp compilation
+cat *.bib > tmp
 
 # Insert the YAML header files needed
-sed -i '' '5i\
+sed -i '' '1i\
 ---\
 ---\
-' ultrasound.bib
+\
+' tmp
 
-# Delete first 4 Mendeley lines
-sed -i '' 1,4d ultrasound.bib
+# Remove old bib files
+rm *.bib
+
+# Rename new bibfile
+mv tmp bibfile.bib
 
 # Replace the bibtex issues for jekyll-scholar
-sed -i '' 's/{{/{/g' ultrasound.bib
-sed -i '' 's/}}/}/g' ultrasound.bib
+sed -i '' 's/{{/{/g' bibfile.bib
+sed -i '' 's/}}/}/g' bibfile.bib
+
